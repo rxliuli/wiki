b01c80a6d6cecaf54fd2f6c36c5fd8b557b1989d
rxliuli <rxliuli@gmail.com>
2021-05-26 22:23:39 +0800

    使用 joplin-blog 导出一些 wiki 内容(其实也包含 blog 的一些内容就是了)

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,13 @@
+const sidebar = require('./sidebar.json')
+
 module.exports = {
-  base: '/awesome-frontend/',
   title: 'Awesome Frontend',
   description: '这是一个自以为是的前端技术选择和推荐网站',
   themeConfig: {
     nav: [
       { text: '首页', link: '/' },
-      { text: '指南', link: '/guide/' },
       { text: 'GitHub', link: 'https://github.com/rxliuli/awesome-frontend' },
     ],
-    sidebar: {
-      '/guide/': ['', '前端资源管理'],
-    },
+    sidebar: [{ title: '首页', path: '/' }, ...sidebar],
   },
 }
