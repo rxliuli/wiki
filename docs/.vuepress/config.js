47f3ab49c5aba70afed5b2cbc8aa0a3126ceac4c
rxliuli <rxliuli@gmail.com>
2021-06-01 13:37:27 +0800

    更新 markdown 配置，自动将换行转换为 <br/>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,9 @@ const sidebar = require('./sidebar.json')
 module.exports = {
   title: 'Awesome Frontend',
   description: '这是一个自以为是的前端技术选择和推荐网站',
+  markdown: {
+    breaks: false,
+  },
   themeConfig: {
     nav: [
       { text: '首页', link: '/' },
