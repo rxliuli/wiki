4da3a812837c0baa384c13f5982b2b7a99075ee7
rxliuli <rxliuli@gmail.com>
2021-07-16 16:41:14 +0800

    chore: 升级 vuepress-vite

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,30 @@
 const sidebar = require('./sidebar.json')
+const { treeMap } = require('@liuli-util/tree')
 
 module.exports = {
   title: 'Awesome Frontend',
   description: '这是一个自以为是的前端技术选择和推荐网站',
-  markdown: {
-    breaks: false,
-  },
+  // markdown: {
+  //   breaks: false,
+  // },
   themeConfig: {
-    nav: [
+    navbar: [
       { text: '首页', link: '/' },
       { text: 'GitHub', link: 'https://github.com/rxliuli/wiki' },
     ],
-    sidebar: [{ title: '首页', path: '/' }, ...sidebar],
+    sidebar: [
+      ...treeMap(
+        sidebar,
+        ({ title: text, path: link, children }) => ({
+          text,
+          link: link ? link + '.html' : link,
+          children,
+        }),
+        {
+          children: 'children',
+          path: 'path',
+        },
+      ),
+    ],
   },
 }
