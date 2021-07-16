5b8bfea990e4cf055f64c19cf5d70e0cbf017d84
rxliuli <rxliuli@gmail.com>
2021-07-16 16:50:19 +0800

    chore: 恢复之前临时注释的配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,9 +4,9 @@ const { treeMap } = require('@liuli-util/tree')
 module.exports = {
   title: 'Awesome Frontend',
   description: '这是一个自以为是的前端技术选择和推荐网站',
-  // markdown: {
-  //   breaks: false,
-  // },
+  markdown: {
+    breaks: false,
+  },
   themeConfig: {
     navbar: [
       { text: '首页', link: '/' },
